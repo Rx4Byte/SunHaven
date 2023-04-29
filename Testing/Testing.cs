0c7af7af8f32e85704f171d487d6d5cdf6354ebd
Rx4Byte <130197051+Rx4Byte@users.noreply.github.com>
2023-04-29 03:20:27 +0200

    just a mod envirement to test stuff

---
+++ Testing/Testing.cs
@@ -0,0 +1,165 @@
+﻿using BepInEx;
+using HarmonyLib;
+using QFSW.QC.Utilities;
+using QFSW.QC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UnityEngine;
+using Wish;
+
+namespace Testing
+{
+    public class PluginInfo
+    {
+        public const string PLUGIN_AUTHOR = "Rx4Byte";
+        public const string PLUGIN_NAME = "Rx4Byte's Test Mod";
+        public const string PLUGIN_GUID = "com.Rx4Byte.Rx4BytesTestMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public partial class CommandExtension : BaseUnityPlugin
+    {
+        private static readonly bool debug = true;
+        #region Awake() | Update() | OnGui()   -   BASE UNITY OBJECT METHODES
+        private void Awake()
+        {
+            if (debug)
+                Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+        }
+        private void Update()
+        {
+
+        }
+        private void OnGUI()
+        {
+
+        }
+        #endregion
+
+        #region Base
+        // get the player for singleplayer/multiplayer
+        public static Player GetPlayerForCommand()
+        {
+            return Player.Instance;
+        }
+        // PRINT FUNCTION
+        private static void LogChat(string text) => QuantumConsole.Instance.LogPlayerText(text);
+        // no chat bubble for test methode
+        private static bool IsTestCommandBubble(string mayCommand)
+        {
+            if (!debug)
+                return false;
+            mayCommand = mayCommand.ToLower();
+            switch (mayCommand)
+            {
+                case "test1":
+                    return true;
+
+                case "test2":
+                    return true;
+
+                case "test3":
+                    return true;
+
+                case "test4":
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+        // check and execute methode
+        public static bool IsTestCommandChat(string mayCommand)
+        {
+            if (!debug)
+                return false;
+            mayCommand = mayCommand.ToLower();
+            switch (mayCommand)
+            {
+                case "test1":
+                    LogChat("[TEST-1]".ColorText(Color.black) + " start".ColorText(Color.white));
+                    TestMethode1();
+                    LogChat("[TEST-1]".ColorText(Color.black) + " end".ColorText(Color.white));
+                    return true;
+
+                case "test2":
+                    LogChat("[TEST-2]".ColorText(Color.black) + " start".ColorText(Color.white));
+                    TestMethode2();
+                    LogChat("[TEST-2]".ColorText(Color.black) + " end".ColorText(Color.white));
+                    return true;
+
+                case "test3":
+                    LogChat("[TEST-3]".ColorText(Color.black) + " start".ColorText(Color.white));
+                    TestMethode3();
+                    LogChat("[TEST-3]".ColorText(Color.black) + " end".ColorText(Color.white));
+                    return true;
+
+                case "test4":
+                    LogChat("[TEST-4]".ColorText(Color.black) + " start".ColorText(Color.white));
+                    TestMethode4();
+                    LogChat("[TEST-4]".ColorText(Color.black) + " end".ColorText(Color.white));
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+        #region two base patches
+        [HarmonyPatch(typeof(Player), nameof(Player.DisplayChatBubble))]
+        class Patch_PlayerDisplayChatBubble
+        {
+            static bool Prefix(ref string text)
+            {
+                if (IsTestCommandBubble(text))
+                    return false;
+                return true;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.SendChatMessage), new[] { typeof(string), typeof(string) })]
+        class Patch_PlayerSendChatMessage
+        {
+            static bool Prefix(string characterName, string message)
+            {
+                if (IsTestCommandChat(message))
+                    return false;  // SEND COMMAND 
+                return true;  // SEND CHAT
+            }
+        }
+        #endregion
+        #endregion
+
+        #region Methode's
+        // TEST 1
+        private static void TestMethode1()
+        {
+
+        }
+        // TEST 2
+        private static void TestMethode2()
+        {
+
+        }
+        // TEST 3
+        private static void TestMethode3()
+        {
+
+        }
+        // TEST 4
+        private static void TestMethode4()
+        {
+
+        }
+        #endregion
+
+        #region Patches
+
+        #endregion
+    }
+}
